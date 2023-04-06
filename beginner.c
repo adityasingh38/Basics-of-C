cfc66cd74fc0c094f3db7fc651da1dfc4c2cd5d1
Aditya Singh <88421625+adityasingh38@users.noreply.github.com>
2023-04-06 14:12:30 +0530

    Update beginner.c

--- beginner.c
+++ beginner.c
@@ -2,4 +2,23 @@
 
 int main()
 {
-int x;
+  int x;
+  float f;
+  char ch;
+   
+  printf("Enter any Integer: ");
+  scanf("%d", &x);
+  printf("\n");
+  
+  printf("Enter any Floating Point: ");
+  scanf("%f", &f);
+  printf("\n");
+  
+  printf("Enter any Character: ");
+  scanf("%c", &ch);
+  printf("\n");
+  
+  printf("Integer: %d\nFloat: %f\n  Character: %c\n", x, f, ch);
+    
+  return 0;
+}
